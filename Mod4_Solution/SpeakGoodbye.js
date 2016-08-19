4ad16907081d7c8e78de8999f446faf221c1285d
Sarah <sweetiejeremy@hotmail.com>
2016-08-19 14:10:40 -0700

    Module 4 Solution

---
+++ Mod4_Solution/SpeakGoodbye.js
@@ -0,0 +1,13 @@
+var byeSpeaker ={};
+
+(function (window) {
+var speakWord = "Good Bye";
+
+
+byeSpeaker.speak=function (name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker=byeSpeaker;
+
+})(window);
+
---
+++ Mod4_Solution/SpeakHello.js
@@ -0,0 +1,12 @@
+var helloSpeaker = {};
+
+(function (window) {
+var speakWord = "Hello";
+
+helloSpeaker.speak= function (name) {
+  console.log(speakWord + " " +	name);
+}
+
+window.helloSpeaker=helloSpeaker;
+})(window);
+
